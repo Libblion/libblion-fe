cd459f8e7a74c32817e37bc706a0263d61002f39
rahman <rahmannurhayadi@gmail.com>
2025-02-14 19:39:27 +0700

    fix(style): menambahkan lazy loading

--- src/components/sections/OfferBooks.vue
+++ src/components/sections/OfferBooks.vue
@@ -1,32 +1,41 @@
 <template>
-     <section class="border md:p-10 p-4 flex items-center flex-col justify-center font-libre-text gap-2">
-        <h1 class="font-semibold text-lg w-full">Offers</h1>
-        <div class="flex flex-row justify-around gap-x-2 items-center h-full border w-full">
-           <figure class="border max-w-64 max-h-90 rounded-t-full grid grid-cols-2 grid-rows-2 overflow-hidden">
-            <img :src="cover1" alt="cover-image">
-            <img :src="cover2" alt="cover-image">
-            <img :src="cover3" alt="cover-image">
-            <img :src="cover4" alt="cover-image">
-           </figure>
-           <figure class="border max-w-64 max-h-90 rounded-b-full grid grid-cols-2 grid-rows-2 overflow-hidden">
-            <img :src="cover5" alt="cover-image">
-            <img :src="cover6" alt="cover-image">
-            <img :src="cover7" alt="cover-image">
-            <img :src="cover8" alt="cover-image">
-           </figure>
-           <figure class="border max-w-64 max-h-90 rounded-t-full grid grid-cols-2 grid-rows-2 overflow-hidden">
-            <img :src="cover9" alt="cover-image">
-            <img :src="cover10" alt="cover-image">
-            <img :src="cover11" alt="cover-image">
-            <img :src="cover12" alt="cover-image">
-           </figure>
-        </div>
-        <div class="text-center">
-         <button class="p-2 px-10 border">
+   <section class="border md:p-10 p-4 flex items-center flex-col justify-center font-libre-text gap-2">
+      <h1 class="font-semibold text-lg w-full">Offers</h1>
+      <div class="flex flex-row justify-around gap-x-2 items-center h-full w-full max-sm:text-xs">
+         <div class="flex flex-col gap-y-5 items-center">
+            <figure class="max-w-64 max-h-90 rounded-t-full grid grid-cols-2 grid-rows-2 overflow-hidden">
+               <img :src="cover1" alt="cover-image" loading="lazy">
+               <img :src="cover2" alt="cover-image" loading="lazy">
+               <img :src="cover3" alt="cover-image" loading="lazy">
+               <img :src="cover4" alt="cover-image" loading="lazy">
+            </figure>
+            <p>Available for loan for 1 month</p>
+         </div>
+         <div class="flex flex-col gap-y-5 items-center" >
+            <figure class="max-w-64 max-h-90 rounded-b-full grid grid-cols-2 grid-rows-2 overflow-hidden">
+               <img :src="cover5" alt="cover-image" loading="lazy">
+               <img :src="cover6" alt="cover-image" loading="lazy">
+               <img :src="cover7" alt="cover-image" loading="lazy">
+               <img :src="cover8" alt="cover-image" loading="lazy">
+            </figure>
+            <p>Available for loan for 2 month</p>
+         </div>
+         <div class="flex flex-col gap-y-5 items-center">
+            <figure class="max-w-64 max-h-90 rounded-t-full grid grid-cols-2 grid-rows-2 overflow-hidden">
+               <img :src="cover9" alt="cover-image" loading="lazy">
+               <img :src="cover10" alt="cover-image" loading="lazy">
+               <img :src="cover11" alt="cover-image" loading="lazy">
+               <img :src="cover12" alt="cover-image" loading="lazy">
+            </figure>
+            <p>Available for loan for 42 days</p>
+         </div>
+      </div>
+      <div class="text-center mt-10">
+         <button class="p-2 px-10 border cursor-pointer hover:bg-slate-100 transition-colors">
             See all
          </button>
-        </div>
-    </section>
+      </div>
+   </section>
 </template>
 
 <script setup>

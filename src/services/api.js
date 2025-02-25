29289f61c198cf99a30a6cce1975ae4fd1660db6
rahman <rahmannurhayadi@gmail.com>
2025-02-25 10:26:03 +0700

    style : fix styleborow table

--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "https://chic-caring-production-2e74.up.railway.app/api/v1",
+  baseURL: "http://localhost:8000/api/v1",
 });
